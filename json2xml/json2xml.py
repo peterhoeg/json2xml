16b1318b9f7393fb2f3e633e366344dbe9d90d0d
Peter Hoeg <peter@hoeg.com>
2021-03-01 15:34:33 +0800

    support setting item name in list

--- json2xml/json2xml.py
+++ json2xml/json2xml.py
@@ -5,22 +5,30 @@ import dicttoxml
 
 class Json2xml:
     def __init__(
-            self, data: str,
-            wrapper: str = "all",
-            pretty: bool = True,
-            attr_type: bool = True
+        self,
+        data: str,
+        wrapper: str = "all",
+        pretty: bool = True,
+        attr_type: bool = True,
+        item_name: str = "item",
     ):
         self.data = data
         self.pretty = pretty
         self.wrapper = wrapper
         self.attr_type = attr_type
+        self.item_name_func = lambda _: item_name
 
     def to_xml(self):
         """
         Convert to xml using dicttoxml.dicttoxml and then pretty print it.
         """
         if self.data:
-            xml_data = dicttoxml.dicttoxml(self.data, custom_root=self.wrapper, attr_type=self.attr_type)
+            xml_data = dicttoxml.dicttoxml(
+                self.data,
+                custom_root=self.wrapper,
+                attr_type=self.attr_type,
+                item_func=self.item_name_func,
+            )
             if self.pretty:
                 return parseString(xml_data).toprettyxml()
             return xml_data
